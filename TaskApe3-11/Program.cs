847f1526d95d9a708abb0f742409aea28e1b0a13
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-05-14 23:15:07 +0400

    TaskApe3

---
+++ TaskApe3-11/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace TaskApe3_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weapon weapon = new Weapon();
+            Console.Write("Capacity: ");
+            weapon.Capacity = Convert.ToInt32(Console.ReadLine());
+            Console.Write("CountG: ");
+            weapon.CountG = Convert.ToInt32(Console.ReadLine());
+
+            bool check = true;
+            do
+            {
+                
+                int selector = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                switch (selector)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("0 - İnformasiya almaq üçün");
+                            Console.WriteLine("1 - Shoot metodu üçün");
+                            Console.WriteLine("2 - Fire metodu üçün");
+                            Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+                            Console.WriteLine("4 - Reload metodu üçün");
+                            Console.WriteLine("5 - ChangeFireMode metodu üçün");
+                            Console.WriteLine("6 - Proqramdan dayandırmaq üçün qısayoldur.");
+
+                        }
+                        break;
+                    case 1:
+                        {
+                            Console.WriteLine("Shoot metod:");
+                            weapon.Shoot();
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Fire  metod:");
+                            weapon.Fire();
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine(" GetRemainBulletCount metod:");
+                            Console.WriteLine(weapon.GetRemainBulletCount());
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("Reolad metod:");
+                            weapon.Reolad();
+                        }
+                        break;
+
+                    case 5:
+                        {
+                            Console.WriteLine("ChangeFireMode");
+                            weapon.ChangeFireMode();
+                        }
+                        break;
+
+                    case 6:
+                        {
+                            check = false;
+                        }
+                        break;
+                }
+
+            } while (check);
+        }
+    }
+}
---
+++ TaskApe3-11/Weapon.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskApe3_11
+{
+    internal class Weapon
+    {
+        public int _capacity;
+        public int Capacity
+        {
+            get { return _capacity; }
+            set
+            {
+                if (value > 0)
+                {
+                    _capacity = value;
+                }
+            }
+        }
+        public int _count;
+        public int CountG
+        {
+            get { return _count; }
+            set
+            {
+                if (_count < Capacity)
+                {
+                    _count = value;
+                }
+            }
+        }
+
+
+        public bool Mode { get; set; }
+
+
+        public void Shoot()
+        {
+            Console.WriteLine(CountG - 1);
+        }
+        public void Fire()
+        {
+            if (Mode == true)
+            {
+                Console.WriteLine(CountG = 0);
+            }
+            else
+            {
+                Console.WriteLine(--CountG);
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return Capacity -= CountG;
+        }
+
+        public void Reolad()
+        {
+            if (CountG < Capacity)
+            {
+                for (int i = CountG; i <= Capacity; i++)
+                {
+                        Console.WriteLine(CountG++);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Drag doludur");
+            }
+
+           
+
+        }
+
+        public void ChangeFireMode()
+        {
+            if (Mode == true)
+            {
+                Console.WriteLine(Mode = false);
+            }
+            else if (Mode == false)
+            {
+                Console.WriteLine(Mode = true);
+            }
+        }
+    }
+}
